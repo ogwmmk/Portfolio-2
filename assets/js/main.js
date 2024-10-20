021ae3d98680771b7b93e049aebc573bb2537299
ogwmmk <mo2418f@gmail.com>
2024-10-21 00:59:49 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-// スクロールするとロゴの色変更
+// スクロールするとheaderの色変更
 $(function () {
   $(window).on("scroll", function () {
     const sliderHeight = $(".header_container").height();
@@ -11,15 +11,54 @@ $(function () {
   });
 });
 
-// スクロールするとロゴの色変更
+// スクロールするとheader_LanguageTubの色変更
 $(function () {
   $(window).on("scroll", function () {
     const sliderHeight = $(".header_container").height();
     if (sliderHeight - 30 < $(this).scrollTop()) {
-      $(".js-header_2").addClass("headerColorScroll_2");
+      $(".js-header-tub").addClass("headerColorScroll-tub");
     } else {
-      $(".js-header_2").removeClass("headerColorScroll_2");
+      $(".js-header-tub").removeClass("headerColorScroll-tub");
     }
   });
 });
 
+
+
+
+// スクロールするとheader_guideの色変更
+
+$(function () {
+  $(window).on("scroll", function () {
+    const sliderHeight = $(".header_container").height();
+    if (sliderHeight - 30 < $(this).scrollTop()) {
+      $(".js-header-guide").addClass("headerColorScroll-guide");
+    } else {
+      $(".js-header-guide").removeClass("headerColorScroll-guide");
+    }
+  });
+});
+
+// ヘッダー スライド
+
+$(document).ready(function() {
+  const images = [
+
+      'assets/img/h-1.jpg',
+      'assets/img/main_1.jpg',
+      'assets/img/h-1.jpg',
+  ];
+
+  let currentIndex = 0;
+
+  function changeBackground() {
+      currentIndex = (currentIndex + 1) % images.length;
+      $('.header_container').css('background-image', `url('${images[currentIndex]}')`);
+  }
+
+  // 5秒ごとに背景を変更
+  setInterval(changeBackground, 5000);
+});
+fin
+
+// ためし2
