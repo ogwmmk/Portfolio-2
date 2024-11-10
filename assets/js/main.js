714a842c98b8109e504cb0a2ed08f89f5f42bdfc
ogwmmk <mo2418f@gmail.com>
2024-11-10 18:43:26 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,24 +41,56 @@ $(function () {
 
 // ヘッダー スライド
 
-$(document).ready(function() {
-  const images = [
+// $(document).ready(function() {
+//   const images = [
 
+//       'assets/img/h-1.jpg',
+//       'assets/img/main_1.jpg',
+//       'assets/img/h-1.jpg',
+//   ];
+
+//   let currentIndex = 0;
+
+//   function changeBackground() {
+//       currentIndex = (currentIndex + 1) % images.length;
+//       $('.header_container').css('background-image', `url('${images[currentIndex]}')`);
+//   }
+
+//   // 5秒ごとに背景を変更
+//   setInterval(changeBackground, 5000);
+// });
+
+
+// ためし2
+const images = [
       'assets/img/h-1.jpg',
       'assets/img/main_1.jpg',
       'assets/img/h-1.jpg',
-  ];
+];
 
-  let currentIndex = 0;
+let currentIndex = 0;
+const headerContainer = document.querySelector('.header_container');
 
-  function changeBackground() {
-      currentIndex = (currentIndex + 1) % images.length;
-      $('.header_container').css('background-image', `url('${images[currentIndex]}')`);
+function changeBackgroundImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  headerContainer.style.background-image = `url(${images[currentIndex]})`;
+}
+
+// 5秒ごとに背景画像を切り替え
+setInterval(changeBackgroundImage, 5000);
+
+
+//ハンバーガーメニュー/
+
+window.addEventListener('scroll', () => {
+  const header = document.getElementById('header_logo');
+  const scrollPosition = window.scrollY;
+  const triggerHeight = 100; // クラスを切り替えるスクロール量（ピクセル単位）
+
+  if (scrollPosition > triggerHeight) {
+      header.classList.add('scrolled');
+  } else {
+      header.classList.remove('scrolled');
   }
-
-  // 5秒ごとに背景を変更
-  setInterval(changeBackground, 5000);
 });
-fin
 
-// ためし2
